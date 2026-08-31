d80816455471dfe6c90a82a1c946114e4e9723e3
zuhooruddin <zuhooruddin055@gmail.com>
2026-08-31 12:26:17 +0500

    resolved siteloader

--- src/components/home/NewsTicker.tsx
+++ src/components/home/NewsTicker.tsx
@@ -73,6 +73,7 @@ export function NewsTicker() {
         })}
       </div>
 
+
       <div className="mt-10 flex justify-center gap-2 px-6 lg:px-[121px]">
         {groups.map((_, i) => (
           <button
